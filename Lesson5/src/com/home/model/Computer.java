ce6bf67e82f4435c03da1bf71a9f6a8cce5d1cc8
Gerovna <gerasimovich1205@gmail.com>
2021-07-05 17:36:28 +0300

    Lesson 5: Домашнее задание

---
+++ Lesson5/src/com/home/model/Computer.java
@@ -0,0 +1,24 @@
+package com.home.model;
+
+public class Computer {
+    String processor;
+    int ram;
+    String hdd;
+    int resource;
+    public Computer(String processor, int ram, String hdd, int resource) {
+        this.processor = processor;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.resource = resource;
+    }
+    // добавляем метод описание(вывод всех полей)
+   @Override
+    public String toString() {
+        return "Computer{"+
+        "processor='"+ processor + '\'' +
+                ",ram='" + ram + '\'' +
+                ",hdd='" + hdd + '\'' +
+                ",resource'" + resource + '\'' +
+                '}';
+    }
+}
